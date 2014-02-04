6bc4236c6d423c9388fc9d2494af5e9613cc4f3e
BaluMonster <taddy.gorp@tiscali.it>
2014-02-04 21:00:48 +0100

    Add: Database.java

---
+++ src/it/multicraft/api/sql/Database.java
@@ -0,0 +1,22 @@
+package it.multicraft.api.sql;
+
+import java.sql.Connection;
+
+import org.bukkit.plugin.Plugin;
+
+public abstract class Database {
+
+	protected Plugin plugin;
+	
+	protected Database(Plugin plugin){
+		this.plugin=plugin;
+	}
+	
+	public abstract Connection openConnection();
+	
+	public abstract boolean checkConnection();
+	
+	public abstract Connection getConnection();
+	
+	public abstract void closeConnection();
+}
