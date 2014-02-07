877af415946e0ac6a1d7dce03e38ad56f9587448
BaluMonster <taddy.gorp@tiscali.it>
2014-02-07 16:48:37 +0100

    Update: Chat.java, MCCommand.java

--- src/it/multicraft/api/Chat.java
+++ src/it/multicraft/api/Chat.java
@@ -2,35 +2,35 @@ package it.multicraft.api;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
+import org.bukkit.command.CommandSender;
 
 public class Chat{
 
 	/**
 	 * Sends a generic message to a player
-	 * @param player
+	 * @param who
 	 * @param message
 	 */
-	public static void genericMsg(Player player, String message){
-		player.sendMessage(message);
+	public static void genericMsg(CommandSender who, String message){
+		who.sendMessage(message);
 	}
 	
 	/**
 	 * Sends an error message (chatcolor: red) to a player
-	 * @param player
+	 * @param who
 	 * @param message
 	 */
-	public static void errorMsg(Player player, String message){
-		player.sendMessage(ChatColor.RED+message);
+	public static void errorMsg(CommandSender who, String message){
+		who.sendMessage(ChatColor.RED+message);
 	}
 	
 	/**
 	 * Sends a notify message (chatcolor: green) to a player
-	 * @param player
+	 * @param who
 	 * @param message
 	 */
-	public static void notifyMsg(Player player, String message){
-		player.sendMessage(ChatColor.GREEN+message);
+	public static void notifyMsg(CommandSender who, String message){
+		who.sendMessage(ChatColor.GREEN+message);
 	}
 	
 	public static void broadcast(String message){
